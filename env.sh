d502b9f28ae052186b9a92fc0ccd8c8709effd28
shaochangxu <shaochangxumail@163.com>
2021-06-14 13:47:55 +0800

    delete sh

--- env.sh
+++
@@ -1,6 +0,0 @@
-# Edit to locate your dataset
-MVS_TRAINING="/home/scx/data/trainingdata/dtu_training"
-BLEND_TRAINING="/data/yhw/dataset_low_res"
-DTU_TESTING="/home/scx/mvsnet/test_data/scan9"
-ETH3D_TESTING="/data/yhw/ETH3d-840/ETH3D-reference-840-510"
-TP_TESTING="/data/yhw/tanksAndtemple"
--- eval.sh
+++
@@ -1,3 +0,0 @@
-dataset_path = "/home/hadoop/data/data/0529/Colmap_In_0/dense"
-
-python colmap2mvsnet.py --dense_folder /home/hadoop/data/data/0529/Colmap_In_0/dense --convert_format --whitelist /home/hadoop/data/data/0529/Colmap_In_0/dense/stereo/fusion.cfg
